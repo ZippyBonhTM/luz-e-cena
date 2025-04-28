face71c94efff41d314c91d424c82dfd33a0c3e7
ZippyBonhTM <zippybont@gmail.com>
2025-04-28 17:06:53 -0300

    Componente Banner: componente e css adicionados, integrado o banner no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import Banner from "./components/Banner"
+
 function App() {
   return (
-    <h1>
-      Olá Mundo
-    </h1>
+    <Banner src="./Imagens/Banner Desktop.png" alt="Banner"/>
   )
 }
 
